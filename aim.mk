8f76babb65a3b265be03a0964990b23ddda9d842
rocknegi <rocknegi53@gmail.com>
2017-04-15 10:03:03 +0000

    aim ready
    Change-Id: I2c6b567450a75b6deaeced6abfb1a92668f8280a

--- aim.mk
+++ aim.mk
@@ -15,14 +15,14 @@
 
 $(call inherit-product, device/xiaomi/kenzo/full_kenzo.mk)
 
-# Inherit some common XOS stuff.
-$(call inherit-product, vendor/xos/config/common.mk)
+# Inherit some common aim stuff.
+$(call inherit-product, vendor/aim/config/common.mk)
 
 # Set those variables here to overwrite the inherited values.
 BOARD_VENDOR := Xiaomi
 PRODUCT_BRAND := Xiaomi
 PRODUCT_DEVICE := kenzo
-PRODUCT_NAME := XOS_kenzo
+PRODUCT_NAME := aim_kenzo
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_MODEL := Redmi Note 3
 TARGET_VENDOR := Xiaomi
@@ -32,7 +32,6 @@ PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 # Use the latest approved GMS identifiers unless running a signed build
 ifneq ($(SIGN_BUILD),true)
 PRODUCT_BUILD_PROP_OVERRIDES += \
-
     BUILD_FINGERPRINT=Xiaomi/kenzo/kenzo:6.0.1/MMB29M/V8.2.1.0.MHOCNDL:user/release-keys \
     PRIVATE_BUILD_DESC="kenzo-user 6.0.1 MMB29M V8.2.1.0.MHOCNDL release-keys"
 
